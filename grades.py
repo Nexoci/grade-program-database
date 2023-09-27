45bf5a3006c3acb172b0a58b7d5d48ba583e93f3
Nexoci <144256092+Nexoci@users.noreply.github.com>
2023-09-27 14:23:26 -0400

    Update grades.py

--- grades.py
+++ grades.py
@@ -1,4 +1,4 @@
-#Name: Konnor Kobelak
+#Name: Konnor Kobelka
 #Date Sep 26th 2023
 #Purpose: Use a database to store student information and be able to acess it
 import sqlite3,time
@@ -44,25 +44,35 @@ def menu():
     done = False
     while not done:
         print("Pick What You Would Like To Do")
-        try:
-            time.sleep(1)
-            #users choice
-            choice =int(input("\nGradebook Menu:\n0: Add Student\n1: List Students\n2: Calculate Course Averages\n3: Calculate Student Averages\n4. Exit\nEnter your choice: "))
-            #dictionary to make choices instead of 100 if statements
-            menus = dict({0:add_student, 1:list_students, 2:"course_average", 3:"student_average"})
-            #if statement to exit
-            if choice ==4:
-                print("Goodbye") 
-                done = True
-            elif choice in options:
-                menus[choice]()
-        except:
-            print("That is not an Option try a number 0-3")
+        #try:
         time.sleep(1)
+        #users choice
+        choice =int(input("\nGradebook Menu:\n0: Add Student\n1: List Students\n2: Calculate Course Averages\n3: Calculate Student Averages\n4. Exit\nEnter your choice: "))
+        #dictionary to make choices instead of 100 if statements
+        menus = dict({0:add_student, 1:list_students, 2:"course_average", 3:student_average})
+        #if statement to exit
+        if choice ==4:
+            print("Goodbye") 
+            done = True
+        elif choice in options:
+            menus[choice]()
+        #except:
+            #print("That is not an Option try a number 0-3")
+        time.sleep(1)
+def student_average():
+    student_name = input("Enter the student's name: ")
+    student_id = input("Enter student Id: ")
+    result = select_db(connection,"student_grades",[f"name='{student_name}'",f"id ={student_id}"]).fetchall()
+    for i in result:
+        print(f"{i[1]}'s average is {(i[2]+i[3]+i[4]+i[5])/4}")
+def course_average():
+    print("gay")    
+    
     
 def list_students():
     results= select_db(connection,"student_grades").fetchall()
     print(results)
+    
 def add_student():
     name = input("Enter student name: ")
     English = float(input("Enter grade for English: "))
